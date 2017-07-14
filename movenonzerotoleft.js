1609147cf0d320b90d5b5066155768b6448e3a92
chaluvadis <surendra.chaluvadi@gmail.com>
2017-07-14 10:47:30 -0400

    logic updated for consistency

--- movenonzerotoleft.js
+++ movenonzerotoleft.js
@@ -19,7 +19,8 @@ const moveZerosToLeft = (x) => {
     let i = 0;
     for (let j = 0; j < x.length; j++) {
         if (x[j] !== 0) {
-            x[i++] = x[j];
+            x[i] = x[j];
+            i = i + 1;
         }
     }
     while (i < x.length) x[i++] = 0;
@@ -27,13 +28,13 @@ const moveZerosToLeft = (x) => {
 }
 
 const moveZerosToRight = (x) => {
-    let i = 0, t,m;
+    let i = 0, t, m;
     for (let j = 0; j < x.length; j++) {
         if (x[j] === 0) {
-            m = i++;
-            t = x[m]
-            x[m] = x[j];
+            t = x[i]
+            x[i] = x[j];
             x[j] = t;
+            i = i + 1;
         }
     }
     return x;
@@ -42,6 +43,6 @@ const moveZerosToRight = (x) => {
 // console.log(moveZerosToLeft([1, 0, 2, 3, 0, 2, 0]));
 // console.log(moveZerosToLeft([6, 7, 0, 2, 3, 0, 2, 0]));
 // console.log(moveZerosToLeft([1, 2, 34, 5, 6, 7, 7, 6]));
-
+console.log(moveZerosToLeft([1, 0, 2, 3, 0, 2, 0]));
 console.log(moveZerosToRight([1, 0, 2, 3, 0, 2, 0]));
 
